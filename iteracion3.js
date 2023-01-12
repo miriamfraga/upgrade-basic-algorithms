f43e222ad5e69b3e88e83f407c6958a7b94e2d96
“miriamfraga” <fragamr@outlook.com>
2023-01-12 20:00:39 +0100

    iteracion3

---
+++ iteracion3.js
@@ -0,0 +1,17 @@
+
+const mult = 5 * 10;
+alert (mult);
+const div = 10/2;
+alert (div);
+const modulo = 15%9;
+alert (modulo);
+
+let y = 10;
+let z = 5;
+let x = y + z;
+console.log(x);
+
+ y = 10; 
+ z = 5;
+x = y * z;
+console.log(x);
