560ff251adf88e60fb839ff7b73d5c3362a68080
“miriamfraga” <fragamr@outlook.com>
2023-01-12 20:18:21 +0100

    iteracion6

---
+++ iteracion6.js
@@ -0,0 +1,21 @@
+// 'use strict'
+// for (let i=0; i <= 9; i++) {
+    // console.log(i);
+    // };
+
+
+// let i = 9;
+// for (i=0; i <= 9; i++) {
+    // if (i %2 == 0){
+        // console.log(i);
+        // }
+//  }
+
+ for (let i=0; i<=10; i++) {
+    if (i < 10) {
+        console.log("Intentando dormir");
+    }else {
+        console.log("Dormido!");
+    }
+}
+ 
